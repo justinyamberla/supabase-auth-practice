b3f0f451e794bc45c932ffe8fffa0ff57d0a1bcf
Justin_Yamberla <justin.yamberla@epn.edu.ec>
2025-06-02 13:03:06 -0500

    Change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import { createBrowserClient } from "@supabase/ssr";
+import {cookies} from "next/headers";
+
+export function createClient() {
+    return createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hola Twitter!
-    </div>
-  );
+    const supabase = createClient();
+    return (
+        <div
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            Hola Twitter!
+        </div>
+    );
 }
