7a74b5158e9466ab8d6e14d1f3edfb301f4a9ae0
Justin_Yamberla <justin.yamberla@epn.edu.ec>
2025-06-03 10:32:20 -0500

    Add
    Adding Supabase Server and Client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,6 @@
-import { createBrowserClient } from "@supabase/ssr";
-import {cookies} from "next/headers";
-
-export function createClient() {
-    return createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
-}
+import React from 'react';
 
 export default function Home() {
-    const supabase = createClient();
     return (
         <div
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
