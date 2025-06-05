938d038af2524f4e0f106b9aaf5e5547b7e43c46
Justin_Yamberla <justin.yamberla@epn.edu.ec>
2025-06-05 15:27:38 -0500

    Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import ClientComponent from "@/components/ClientComponent";
 
 export default function Home() {
     return (
-        <div
-            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            Hola Práctica de Supabase Auth!
-        </div>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <ClientComponent />
+        </main>
     );
 }
--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -2,12 +2,17 @@ import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
 
 export default async function PrivatePage() {
-    const supabase = await createClient()
 
+    const supabase = await createClient()
     const { data, error } = await supabase.auth.getUser()
+
     if (error || !data?.user) {
         redirect('/login')
     }
 
-    return <p>Hello {data.user.email}</p>
+    return (
+        <p className="flex min-h-screen flex-col items-center justify-between p-24">
+            Hello
+        </p>
+    );
 }
